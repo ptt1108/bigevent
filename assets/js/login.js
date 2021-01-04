c0cec23d490a695b2db2b0a42ce2f76cd993920d
kongbai <254197689@qq.com>
2021-01-04 10:05:30 +0800

    登录&注册页

---
+++ assets/js/login.js
@@ -0,0 +1,70 @@
+$(".register-btn").on("click", function () {
+    $(".login").hide();
+    $(".register").show();
+})
+
+$(".login-btn").on("click", function () {
+    $(".register").hide();
+    $(".login").show();
+})
+
+// 表单验证
+// 注册
+let form = layui.form;
+let layer = layui.layer;
+form.verify({
+    pass: [
+      /^[\S]{6,12}$/
+      ,'密码必须6到12位，且不能出现空格'
+    ],
+    
+    repwd: function(value, item){ //value：表单的值、item：表单的DOM对象
+        let pwd = $(".register [name = password]").val().trim();
+        if(value !== pwd){
+            return "密码不一致";
+        }
+      }
+  });
+
+  
+ $.ajaxPrefilter(function (options) {
+        options.url = "http://ajax.frontend.itheima.net" + options.url;
+    })
+
+$("#register-form").on("submit", function(e){
+    e.preventDefault();
+    let data = $(this).serialize();
+    $.ajax({
+        type:"POST",
+        url:"/api/reguser",
+        data:data,
+        success:function(res){
+            if(res.status !== 0){
+                return layer.msg(res.message);
+            }
+            layer.msg("注册成功！");
+            $(".login-btn").click();
+        },
+        error: function(){
+            return layer.msg("服务器错误！");
+        }
+    })
+})
+
+// 登录
+$("#login-form").on("submit", function(e){
+    e.preventDefault();
+    let data = $(this).serialize();
+    $.ajax({
+        type:"POST",
+        url:"/api/login",
+        data,
+        success:function(res){
+            if(res.status !== 0){
+                return layer.msg(res.message);
+            }
+            layer.msg("登录成功！");
+            location.href = "/home/index.html";
+        }
+    })
+})
